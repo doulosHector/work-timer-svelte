dd60449c4a31117d50285f1f5df6248c25be3ca0
Héctor Ortiz <doulos.emmanuel@gmail.com>
2023-10-27 18:27:40 -0600

    feat: add check to add food hour to est end time

--- src/components/atoms/EstimatedEndTime.svelte
+++ src/components/atoms/EstimatedEndTime.svelte
@@ -6,12 +6,21 @@
 	export let breakTimeInMinutes;
 	export let workTimeInMinutes;
 
+	let addFoodHour = false;
+
+	$: foodHour = addFoodHour ? 60 : 0;
 	$: targetLeftInSeconds = targetTimeInSeconds - timeElapsedInSeconds;
 	$: targetLeftInMinutes = targetLeftInSeconds > 0 ? targetLeftInSeconds / 60 : 0;
 	$: breaksLeft = workTimeInMinutes > 0 ? Math.floor(targetLeftInMinutes / workTimeInMinutes) : 0;
 	$: breakTimeLeftInMinutes = breaksLeft * breakTimeInMinutes;
-	$: totalTimeLeftInMinutes = Math.floor(targetLeftInMinutes + breakTimeLeftInMinutes);
+	$: totalTimeLeftInMinutes = Math.floor(targetLeftInMinutes + breakTimeLeftInMinutes) + foodHour;
 	$: estimatedEndTime = dayjs().add(totalTimeLeftInMinutes, 'minute').format('hh:mm a');
 </script>
 
 <p>Est. end time: {estimatedEndTime}</p>
+<div>
+	<label for="addFoodHour">
+		Add food hour
+		<input type="checkbox" id="addFoodHour" bind:checked={addFoodHour} />
+	</label>
+</div>
