bb582fa165f6f9c7f85d8079899716dbf0a82c62
Héctor Ortiz <doulos.emmanuel@gmail.com>
2023-12-27 10:15:45 -0600

    refactor: calculate est end time with reactive vars

--- src/components/atoms/EstimatedEndTime.svelte
+++ src/components/atoms/EstimatedEndTime.svelte
@@ -7,30 +7,16 @@
 	export let workTimeInMinutes;
 
 	let addFoodHour = false;
-	let targetLeftInSeconds = 0;
-	let targetLeftInMinutes = 0;
-	let breaksLeft = 0;
-	let breakTimeLeftInMinutes = 0;
-	let totalTimeLeftInMinutes = 0;
-	let estimatedEndTime = 0;
 
 	$: foodHour = addFoodHour ? 60 : 0;
+	$: targetLeftInSeconds = targetTimeInSeconds - workTimeElapsedInSeconds;
+	$: targetLeftInMinutes = targetLeftInSeconds > 0 ? targetLeftInSeconds / 60 : 0;
 
-	$: {
-		// This reasign estimatedEndTime when currentTimer
-		// or foodHour changes to have the most accurate
-		// estimatedEndTime
-		if (workTimeElapsedInSeconds || foodHour) {
-			targetLeftInSeconds = targetTimeInSeconds - workTimeElapsedInSeconds;
-			targetLeftInMinutes = targetLeftInSeconds > 0 ? targetLeftInSeconds / 60 : 0;
+	$: breaksLeft = workTimeInMinutes > 0 ? Math.round(targetLeftInMinutes / workTimeInMinutes) : 0;
+	$: breakTimeLeftInMinutes = breaksLeft * breakTimeInMinutes;
 
-			breaksLeft = workTimeInMinutes > 0 ? Math.round(targetLeftInMinutes / workTimeInMinutes) : 0;
-			breakTimeLeftInMinutes = breaksLeft * breakTimeInMinutes;
-
-			totalTimeLeftInMinutes = Math.round(targetLeftInMinutes + breakTimeLeftInMinutes + foodHour);
-			estimatedEndTime = dayjs().add(totalTimeLeftInMinutes, 'minute').format('hh:mm a');
-		}
-	}
+	$: totalTimeLeftInMinutes = Math.round(targetLeftInMinutes + breakTimeLeftInMinutes + foodHour);
+	$: estimatedEndTime = dayjs().add(totalTimeLeftInMinutes, 'minute').format('hh:mm a');
 </script>
 
 <p>Est. end time: {estimatedEndTime}</p>
